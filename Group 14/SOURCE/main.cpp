bf357bce8d0cf7955d5a2e9fa3f3df5602d10c82
TanTran <tantran.300803@gmail.com>
2022-11-22 09:07:25 +0700

    done

--- Group 14/SOURCE/main.cpp
+++ Group 14/SOURCE/main.cpp
@@ -5,26 +5,31 @@ int main(int argc, char** argv) {
     if(argc == 5){
         if(strcmp(argv[1], "-a") == 0){
             if(checkGivenInput(argv[3])){
-                cout << "cmd1" << endl;
+                cout << "CMD1" << endl;
+                cout << "-----------------\n";
                 cmd1(argv[2], argv[3], argv[4]);
             }
             else if(!checkGivenInput(argv[3])) {
-                cout << "cmd3" << endl;
+                cout << "CMD3" << endl;
+                cout << "-----------------\n";
                 cmd3(argv[2], argv[3], argv[4]);
             }
         }
         else if(strcmp(argv[1], "-c") == 0) {
-            cout << "cmd4" << endl;
+            cout << "CMD4" << endl;
+            cout << "-----------------\n";
             cmd4(argv[2], argv[3], argv[4]);
         }
     }
     else if(argc == 6){
         if(strcmp(argv[1], "-a") == 0) {
-            cout << "cmd2" << endl;
+            cout << "CMD2" << endl;
+            cout << "-----------------\n";
             cmd2(argv[2], argv[3], argv[4], argv[5]);
         }
         else if(strcmp(argv[1], "-c") == 0){
-            cout << "cmd5" << endl;
+            cout << "CMD5" << endl;
+            cout << "-----------------\n";
             cmd5(argv[2], argv[3], argv[4], argv[5]);
         }
     }
--- README.md
+++ README.md
@@ -11,3 +11,15 @@
 
 - [Raw file](Group%2014/Report.docx)
 - [One Drive](https://studenthcmusedu-my.sharepoint.com/:w:/g/personal/21120566_student_hcmus_edu_vn/EYIiyGO1ELdFhDqDObcCXJAByTshN0Lm6TeFdLTMZ4WHJQ?e=JiKwfL)
+
+<!-- main.exe -a selection-sort 300000 -both
+main.exe -a insertion-sort 300000 -both
+main.exe -a bubble-sort 300000 -both
+main.exe -a heap-sort 300000 -both
+main.exe -a merge-sort 300000 -both
+main.exe -a quick-sort 300000 -both
+main.exe -a radix-sort 300000 -both
+main.exe -a shaker-sort 300000 -both
+main.exe -a shell-sort 300000 -both
+main.exe -a counting-sort 300000 -both
+main.exe -a flash-sort 300000 -both -->
